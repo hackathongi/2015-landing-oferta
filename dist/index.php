36dff20924bdc872643528ac28f81c9ec41f3832
Bernabé Borrero <bernabe@volcanicinternet.com>
2015-03-28 19:37:48 +0100

    a

--- dist/index.php
+++ dist/index.php
@@ -18,8 +18,13 @@
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
     $result = curl_exec($ch);
     curl_close($ch);
+
+    print_r('<pre>');
+    print_r($result);
+    print_r('</pre>');
 
     $job = json_decode($result, true);
 

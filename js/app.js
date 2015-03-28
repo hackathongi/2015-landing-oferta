9148e8723fa7b4d22c57d481aacc004a420f92b0
carlos <carlosvelasco69@hotmail.com>
2015-03-28 10:14:37 +0100

    HACKA #comment canvi - passat item de app job a la vista de landing

--- js/app.js
+++ js/app.js
@@ -2,7 +2,18 @@
 var myApp = angular.module('hackathonApp',[]);
 
 myApp.controller('LandingController', ['$scope', '$http', function($scope, $http) {
-  $scope.greeting = 'Hola!';
   
-  $scope.items = [1,2,3,4];
+	$scope.job = {};
+  
+	// Simple GET request example :
+	$http.get('https://demo3532506.mockable.io/items').
+	  success(function(data, status, headers, config) {
+		// this callback will be called asynchronously
+		// when the response is available
+		$scope.job = data;
+	  }).
+	  error(function(data, status, headers, config) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	  });
 }]);

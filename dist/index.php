e6db16c75581ced1031a715d8cc5f0eb49188d91
Bernabé Borrero <bernabe@volcanicinternet.com>
2015-03-28 20:22:20 +0100

    ssl

--- dist/index.php
+++ dist/index.php
@@ -20,7 +20,7 @@
     curl_setopt($ch, CURLOPT_SSL_VERSION, 3);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
+    // curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_VERBOSE, true);
     $result = curl_exec($ch);
     curl_close($ch);

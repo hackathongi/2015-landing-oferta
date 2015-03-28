6a536729283bb68f3dbbeaff877e693e9a397617
Bernabé Borrero <bernabe@volcanicinternet.com>
2015-03-28 20:09:03 +0100

    verbose

--- dist/index.php
+++ dist/index.php
@@ -19,6 +19,7 @@
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_URL,$url);
     curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
     $result = curl_exec($ch);
     curl_close($ch);
 
